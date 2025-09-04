0a319ea7438bf5fa229c7c6716744ab3d4b4bbd9
ariekas <140879155+ariekas@users.noreply.github.com>
2025-09-04 15:31:05 +0700

    Update MiniTask4.js

--- MiniTask4.js
+++ MiniTask4.js
@@ -1,24 +1,24 @@
-// buatkan fungsi untuk menghitung 
+// buatkan fungsi untuk menghitung
 // 4 fungsi
 // confersi suhu celsius ke Fahrenheit
 // celsius ke reamur
 // celsius ke celvin
 // outpun hasil korversikan kan dengan format yang benar
 
-function Fahrenheit(c) {
-    const FahrenheitKonvert = (c * 9/5) + 32 
-    return FahrenheitKonvert
+function fahrenheit(c) {
+  const FahrenheitKonvert = (c * 9) / 5 + 32;
+  return FahrenheitKonvert;
 }
 
-const reamur = function(c){
-    const reamurKonvert = (4/5)*c 
-    return reamurKonvert
-}
+const reamur = function (c) {
+  const reamurKonvert = (4 / 5) * c ;
+  return reamurKonvert;
+};
 
 const kelvin = (c) => {
-    const kelvinKonvert = c + 273.15
-    return kelvinKonvert
-}
+  const kelvinKonvert = c + 273.15;
+  return kelvinKonvert;
+};
 
 // function ouput(c){
 //     console.log("Hasil konversi Celsius " + c + " ke Fahrenheit adalah:" + Fahrenheit(c))
@@ -26,16 +26,11 @@ const kelvin = (c) => {
 //     console.log("Hasil konversi Celsius " + c + " ke kelvin adalah:" + kelvin(c))
 // }
 
-function out(c, cb){
-   
-    console.log("Celsius "+ c +" Hasil konversinya: " + cb(c))
-    return
+function out(c, cb1, cb2, cb3) {
+  console.log("Hasil konversi Celsius " + c + " ke Fahrenheit adalah: " + cb1(c));
+  console.log("Hasil konversi Celsius " + c + " ke reamur adalah: " + cb2(c));
+  console.log("Hasil konversi Celsius " + c + " ke kelvin adalah: " + cb3(c));
+  return;
 }
 
-// ouput(50)
-
-
-out(5, Fahrenheit)
-out(5, reamur)
-out(5, kelvin)
-
+out(5, fahrenheit, reamur, kelvin);
