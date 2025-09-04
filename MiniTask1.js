cb9c5ab134c1519f793c6d5083fc5dce5e70d4f9
ariekas <140879155+ariekas@users.noreply.github.com>
2025-09-04 10:14:25 +0700

    Add files via upload

---
+++ MiniTask1.js
@@ -0,0 +1,21 @@
+const bio = {
+    name : "ari",
+    age: 18
+}
+
+const extraInfo = {
+    skills:  ["developer", "painting"],
+    education: [
+        {
+            name: "SMP 1 Bali",
+            year: 2025
+        }
+    ]
+}
+
+const profile = {
+    ...bio,
+    ...extraInfo
+}
+
+console.log(profile.skills[1])
